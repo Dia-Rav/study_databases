58123edf262c7bbbf0b6ff55d7c2fe958579cafc
Ravilova Diana <90519131+Dia-Rav@users.noreply.github.com>
2022-11-24 10:42:58 +0300

    Update Diana_5_9_13_17_21_25_29_33.sql

--- HW_1/Diana_5_9_13_17_21_25_29_33.sql
+++ HW_1/Diana_5_9_13_17_21_25_29_33.sql
@@ -1,26 +1,28 @@
 /*5.	Рассчитать кол-во уникальных чеков
 
-SELECT COUNT (*) as '№5 кол-во уникальных чеков'
+SELECT COUNT (distinct checkId) as '№5 кол-во уникальных чеков'
 FROM distributor.singleSales
 WHERE branchName = 'Екатеринбург' and dateId BETWEEN '2014-01-01' and '2014-01-31'
 
 9.	Отсортировать данные по убыванию продаж в чеках, со следующими условиями
 
-SELECT fullname as 'Менеджер',  salesRub as 'Сумма продажи отсуртированная по убыванию', sales as 'Объем продажи', companyName as 'Контрагент', itemId as 'Артикул товара'
+SELECT fullname as 'Менеджер',  sum(salesRub) as sumSales
 FROM distributor.singleSales
 WHERE branchName = 'Екатеринбург' and dateId BETWEEN '2014-01-01' and '2014-01-31'
-ORDER BY salesRub DESC
+group by checkId
+ORDER BY sumSales DESC
 
 13.	Получить информацию о № чека с максимальной суммой платежа, по следующим условиям
 
-SELECT TOP 1 checkId as '№ чека с максимальной суммой платежа'
+SELECT TOP 1 checkId as '№ чека с максимальной суммой платежа', sum(salesRub) as sumSales
 FROM distributor.singleSales
 WHERE branchName = 'Екатеринбург' and dateId BETWEEN '2014-01-01' and '2014-01-31'
-ORDER BY salesRub DESC
+group by checkId
+ORDER BY sumSales DESC
 
 17.	Сколько обслуживает клиентов каждый менеджер со следующими условиями
 
-SELECT fullname as 'Менеджер', COUNT(*)
+SELECT fullname as 'Менеджер', COUNT(distinct checkId)
 FROM distributor.singleSales
 WHERE branchName = 'Екатеринбург' and dateId BETWEEN '2014-01-01' and '2014-01-31'
 GROUP BY  fullname
