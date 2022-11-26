14eba1bd338e76380f889fe45348db5ccce54c70
Ravilova Diana <90519131+Dia-Rav@users.noreply.github.com>
2022-11-26 12:40:39 +0300

    new comments

--- HW_1/Diana_5_9_13_17_21_25_29_33.sql
+++ HW_1/Diana_5_9_13_17_21_25_29_33.sql
@@ -1,10 +1,10 @@
-/*5.	Рассчитать кол-во уникальных чеков
+--5.	Рассчитать кол-во уникальных чеков
 
 SELECT COUNT (distinct checkId) as '№5 кол-во уникальных чеков'
 FROM distributor.singleSales
 WHERE branchName = 'Екатеринбург' and dateId BETWEEN '2014-01-01' and '2014-01-31'
 
-9.	Отсортировать данные по убыванию продаж в чеках, со следующими условиями
+--9.	Отсортировать данные по убыванию продаж в чеках, со следующими условиями
 
 SELECT fullname as 'Менеджер',  sum(salesRub) as sumSales
 FROM distributor.singleSales
@@ -12,7 +12,7 @@ WHERE branchName = 'Екатеринбург' and dateId BETWEEN '2014-01-01' an
 group by checkId
 ORDER BY sumSales DESC
 
-13.	Получить информацию о № чека с максимальной суммой платежа, по следующим условиям
+--13.	Получить информацию о № чека с максимальной суммой платежа, по следующим условиям
 
 SELECT TOP 1 checkId as '№ чека с максимальной суммой платежа', sum(salesRub) as sumSales
 FROM distributor.singleSales
@@ -20,14 +20,14 @@ WHERE branchName = 'Екатеринбург' and dateId BETWEEN '2014-01-01' an
 group by checkId
 ORDER BY sumSales DESC
 
-17.	Сколько обслуживает клиентов каждый менеджер со следующими условиями
+--17.	Сколько обслуживает клиентов каждый менеджер со следующими условиями
 
 SELECT fullname as 'Менеджер', COUNT(distinct checkId)
 FROM distributor.singleSales
 WHERE branchName = 'Екатеринбург' and dateId BETWEEN '2014-01-01' and '2014-01-31'
 GROUP BY  fullname
 
-21.	Какой менеджер, принес максимальную выручку в филиале за определенный месяц
+--21.	Какой менеджер, принес максимальную выручку в филиале за определенный месяц
 
 SELECT TOP 1 fullname as 'Менеджер', sum(salesRub) as 'Sum of sales'
 FROM distributor.singleSales
@@ -35,16 +35,16 @@ WHERE branchName = 'Екатеринбург' and dateId BETWEEN '2014-01-01' an
 GROUP BY  fullname
 ORDER BY 'Sum of sales' DESC
 
-25.	Найти с помощью неточного поиска, следующие наименования компании  (пусть будет ремонт)
-*/
+--25.	Найти с помощью неточного поиска, следующие наименования компании  (пусть будет ремонт)
+
 
 select top 10* 
 from distributor.singleSales
 where companyName like '%РЕМОНТ%'
 
 
-/*
-29.	Рассчитать среднее кол-во артикулов в чеке, в разрезе выделенного менеджера, по следующим условиями
+
+--29.	Рассчитать среднее кол-во артикулов в чеке, в разрезе выделенного менеджера, по следующим условиями
 SELECT  AVG(Counting) as 'Среднее количество артикулов в чеке'
 FROM (
         SELECT COUNT(itemId) as 'Counting', checkId
@@ -54,7 +54,7 @@ FROM (
     ) 
     as NEW_avg
 
-33.	Найти сумму продаж по менеджерам, со следующими условиями
+--33.	Найти сумму продаж по менеджерам, со следующими условиями
 
 SELECT fullname as 'Менеджер', sum(salesRub) as 'Sum of sales'
 FROM distributor.singleSales
@@ -62,4 +62,3 @@ WHERE branchName = 'Екатеринбург' and dateId BETWEEN '2014-01-01' an
 GROUP BY  fullname
 ORDER BY 'Sum of sales' DESC
 
-*/
